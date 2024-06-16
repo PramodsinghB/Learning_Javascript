48794b072665bed0b882222a2e453f080af67ed7
PramodsinghB <bhadoriyapramodsingh038@gmail.com>
2024-06-16 11:35:40 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const func=56656;
+let city="ahmedabad";
+var food="icecream"
+myName="Pramod";
+let state;
+
+// func=444;
+city="delhi";
+food="chocolate"
+myName="chotu";
+
+console.table([func,city,food,myName,state]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not brower
+
+console.log(3
+    +3)   // code readability should be high
+    
+    console.log("chotu"); 
+
+    //number=> 2to power 53
+    //bigInt
+    //string
+    //boolean
+    //null
+    //undefined
+    //symbol=> unique //probably will be using it while doing react, kindoff shows some uniquness
+
+    //objects
+
+    let city="ahmedabad";
+
+
+    console.log(typeof "chotu");
+    console.log(typeof city);
+    console.log(typeof 45555);
