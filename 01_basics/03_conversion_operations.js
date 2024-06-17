43f9970c5941e3faaab015e855735774323bdd47
PramodsinghB <bhadoriyapramodsingh038@gmail.com>
2024-06-17 05:00:15 +0000

    conversion,operations

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,30 @@
+let score="33"
+let scoreo="myntra"
+//console.log(score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//convrt score to number from string
+//console.log(typeof valueInNumber) //TYPE=>NUM
+//console.log(valueInNumber); //NaN- not a number
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//************ Operations*************** */
+
+let value=3;
+let negValue=-value
+//console.log(negValue);
+
+ let gameCounter=100;
+ //let game=gameCounter++;
+ let game1=++gameCounter;
+ //console.log(gameCounter);
+ //console.log(game);
+ console.log(game1);
+
+ // some basic shit, we are good here!!
